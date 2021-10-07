27e70eea4ab8004f9f4b7696f71ce6262c73dac9
James Lefrère <james.lefrere@mstable.org>
2021-10-07 14:51:10 +0200

    Add dist (avoid publishing package)

---
+++ dist/cjs/onboard.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('regenerator-runtime/runtime');
+
+var onboard = require('./onboard-0fd93d73.js');
+
+require('bignumber.js');
+
+require('bnc-sdk');
+
+require('bowser');
+
+module.exports = onboard.init;
---
+++ dist/esm/onboard.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime/runtime';
+export { i as default } from './onboard-fe0e620a.js';
+import 'bignumber.js';
+import 'bnc-sdk';
+import 'bowser';
